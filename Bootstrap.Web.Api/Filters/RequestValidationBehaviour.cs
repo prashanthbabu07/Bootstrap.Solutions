790f30528cff27166fd98ecd01855e8d055c393e
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-05-18 18:26:20 +0530

    Update RequestValidationBehaviour.cs

--- Bootstrap.Web.Api/Filters/RequestValidationBehaviour.cs
+++ Bootstrap.Web.Api/Filters/RequestValidationBehaviour.cs
@@ -6,29 +6,29 @@ namespace Bootstrap.Web.Api.Filters;
 
 public class RequestValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
-	private readonly IEnumerable<IValidator<TRequest>> _validators;
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-	public RequestValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
-	{
-		_validators = validators;
-	}
+    public RequestValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
 
-	public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
-	{
-		//Console.WriteLine($"Validator::{request.GetType().Name}");
-		//Console.WriteLine(_validators.Count());
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        //Console.WriteLine($"Validator::{request.GetType().Name}");
+        //Console.WriteLine(_validators.Count());
+        var context = new ValidationContext<TRequest>(request);
+        var failures = _validators
+                        .Select(v => v.Validate(context))
+                        .SelectMany(r => r.Errors)
+                        .Where(f => f != null)
+                        .ToList();
 
+        if (failures.Count != 0)
+        {
+            throw new ValidationFailedException(failures);
+        }
 
-		var context = new ValidationContext<TRequest>(request);
-		var failures = _validators
-						.Select(v => v.Validate(context))
-						.SelectMany(r => r.Errors)
-						.Where(f => f != null)
-						.ToList();
-
-		if (failures.Count != 0)
-			throw new ValidationFailedException(failures);
-
-		return next();
-	}
+        return next();
+    }
 }
